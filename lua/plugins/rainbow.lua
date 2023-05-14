e5af77ea4b092fd84a10fd260be6c26cda9c49b2
Dap9 <tandonjayant@gmail.com>
2023-05-14 15:42:57 -0700

    Add rainbow parens

---
+++ lua/plugins/rainbow.lua
@@ -0,0 +1,6 @@
+-- Needed separately from 'dependencies' in treesitter for some reason, not sure why
+local M = {
+  "HiPhish/nvim-ts-rainbow2",
+}
+
+return M
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,10 @@
 local M = {
   'nvim-treesitter/nvim-treesitter',
+  dependencies = {
+    "HiPhish/nvim-ts-rainbow2",
+  },
   build = ":TSUpdate",
-  event = "BufReadPost",
+  event = { "BufReadPost", "BufNewFile" },
   opts = {
     ensure_installed = { "c", "cpp", "lua", "rust", "python", "bash", "java" },
     -- ensure_installed = 'maintained', -> depretiated
@@ -13,17 +16,17 @@ local M = {
     rainbow = {
       enable = true,
       -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-      extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-      max_file_lines = nil, -- Do not enable for files with more than n lines, int
-      -- colors = {}, -- table of hex strings
-      -- termcolors = {} -- table of colour name strings
+      -- Which query to use for finding delimiters
+      query = 'rainbow-parens',
+      -- Highlight the entire buffer all at once
+      strategy = require('ts-rainbow').strategy.global,
     },
     indent = {
       enable = true,
       disable = {}
     },
     disable = function(lang, buf)
-      local max_filesize = 100 * 1024   -- 100 KB, prevent parsing on extrememly big files
+      local max_filesize = 100 * 1024 -- 100 KB, prevent parsing on extrememly big files
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_filesize then
         return true
