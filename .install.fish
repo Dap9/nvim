6789eb1d2c808121c19f9534a637879b9f8b5bf2
Dap9 <tandonjayant@gmail.com>
2024-07-01 18:24:41 -0400

    Update clone branch function name

--- .install.fish
+++ .install.fish
@@ -2,7 +2,7 @@
 
 function __install -d "Install neovim"
   printf "Installing Neovim...\n"
-  checkout_branch "https://github.com/neovim/neovim" /tmp/nvim stable && \
+  clone_branch "https://github.com/neovim/neovim" /tmp/nvim stable && \
     pushd /tmp/nvim && \
     make CMAKE_BUILD_TYPE=RelWithDebInfo && \
     sudo make install
