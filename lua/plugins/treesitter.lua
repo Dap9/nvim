1a08173dfc02b70d1e55cb250c7a36ef144477e9
Dap9 <tandonjayant@gmail.com>
2025-01-22 01:32:06 -0800

    Add init function for treesitter based on LazyVim setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,16 @@ local M = {
   'nvim-treesitter/nvim-treesitter',
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile", "VeryLazy" },
+  init = function(plugin)
+    -- Taken from LazyVim
+    -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+    -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+    -- no longer trigger the **nvim-treesitter** module to be loaded in time.
+    -- Luckily, the only things that those plugins need are the custom queries, which we make available
+    -- during startup.
+    require("lazy.core.loader").add_to_rtp(plugin)
+    require("nvim-treesitter.query_predicates")
+  end,
   -- load treesitter early when opening a file from the cmdline
   lazy = vim.fn.argc(-1) == 0,
   cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
