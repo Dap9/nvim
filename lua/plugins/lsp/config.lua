b1aee28bdea832647485feb5e3cb1eb6c5d31f48
Dap9 <tandonjayant@gmail.com>
2025-01-23 01:36:10 -0800

    Remove server list from lsp/config.lua

--- lua/plugins/lsp/config.lua
+++ lua/plugins/lsp/config.lua
@@ -1,21 +1,5 @@
 local M = {}
 
--- Look at https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
--- for a mapping between lsp server name & mason package name when adding a new LSP server
-M.servers = {
-  'basedpyright',
-  'clangd',
-  'rust_analyzer',
-  'lua_ls',
-  'bashls',
-  'bashls',
-  'gopls',
-  'neocmake',
-  'markdown_oxide',
-  -- 'pylsp',
-  -- 'java_language_server',
-}
-
 M.on_attach = function()
   local opts = { noremap = true, silent = true }
   -- Mappings.
