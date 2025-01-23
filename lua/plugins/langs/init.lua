9e8148d1c41dd0b095b1c1d91524d81553e6d21e
Dap9 <tandonjayant@gmail.com>
2025-01-23 01:26:41 -0800

    Move langs/ & mason out of lsp/ since it may have non-lsp settings (e.g. formatters)

---
+++ lua/plugins/langs/init.lua
@@ -0,0 +1,11 @@
+local M = {
+  require("plugins.langs.lua"),
+  require("plugins.langs.python"),
+  require("plugins.langs.golang"),
+  require("plugins.langs.rust"),
+  require("plugins.langs.cpp"),
+  require("plugins.langs.js"),
+  require("plugins.langs.cmake"),
+}
+
+return M
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,7 +1,5 @@
 local M = {
-  require("plugins.lsp.mason"),
   require("plugins.lsp.lspconfig"),
-  require("plugins.lsp.langs"),
 }
 
 return M
