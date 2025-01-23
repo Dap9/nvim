b28ea539bc88267aa8a7e72639e832d16b0cd61a
Dap9 <tandonjayant@gmail.com>
2025-01-23 03:05:19 -0800

    Add catppuccin as a colorscheme option

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,5 @@
 local M = {
-  kanagawa = {
+  {
     "rebelot/kanagawa.nvim",
     lazy = false,
     priority = 1000,
@@ -16,11 +16,11 @@ local M = {
 
     config = function(_, opts)
       require("kanagawa").setup(opts);
-      vim.cmd([[colorscheme kanagawa-dragon]])
+      vim.cmd.colorscheme([[kanagawa]])
     end,
   },
 
-  tokyonight = {
+  {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
@@ -29,11 +29,57 @@ local M = {
       style = "night",
     },
 
-    config = function (_, opts)
+    config = function(_, opts)
       require("tokyonight").setup(opts);
-      vim.cmd([[colorscheme tokyonight]]);
+      vim.cmd.colorscheme([[tokyonight]]);
     end
-  }
+  },
+
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    cond = vim.g.colorscheme == "catppuccin",
+    opts = {
+      flavor = "auto",
+      background = {
+        dark = "mocha",
+        light = "frappe",
+      },
+      integrations = {
+        telescope = {
+          enabled = true,
+        },
+        gitsigns = true,
+        aerial = true,
+        mason = true,
+        native_lsp = {
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+          inlay_hints = {
+            background = true,
+          },
+        },
+        -- raibow_delimiters = true,
+      },
+    },
+
+    config = function (_, opts)
+      require("catppuccin").setup(opts);
+      vim.cmd.colorscheme([[catppuccin]]);
+    end,
+  },
 }
 
-return M[vim.g.colorscheme]
+return M
