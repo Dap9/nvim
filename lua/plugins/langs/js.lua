9b7308fc184ea473adc288bbfdf99556852f7a0d
Dap9 <tandonjayant@gmail.com>
2025-01-24 02:27:41 -0800

    Add prettier formatter

--- lua/plugins/langs/js.lua
+++ lua/plugins/langs/js.lua
@@ -1,14 +1,55 @@
+-- Taken from LazyVim
+local supported = {
+  "css",
+  "graphql",
+  "handlebars",
+  "html",
+  "javascript",
+  "javascriptreact",
+  "json",
+  "jsonc",
+  "less",
+  "markdown",
+  "markdown.mdx",
+  "scss",
+  "typescript",
+  "typescriptreact",
+  "vue",
+  "yaml",
+}
+
 local M = {
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-	ts_ls = {
+        ts_ls = {
           on_attach = require("plugins.lsp.config").on_attach,
-	}
+        },
       },
     },
-  }
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "prettier",
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    ---@module "conform"
+    ---@param opts conform.setupOpts
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      for _, ft in ipairs(supported) do
+        opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+        -- TODO: Below has a warning, but it works. Why? No idea.
+        table.insert(opts.formatters_by_ft[ft], "prettier")
+      end
+    end,
+  },
 }
 
 return M
