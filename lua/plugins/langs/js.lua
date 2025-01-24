189908c08e5f7b60a0c26da02638470c33c33815
Dap9 <tandonjayant@gmail.com>
2025-01-24 02:49:05 -0800

    Add warning ignore

--- lua/plugins/langs/js.lua
+++ lua/plugins/langs/js.lua
@@ -45,7 +45,7 @@ local M = {
       opts.formatters_by_ft = opts.formatters_by_ft or {}
       for _, ft in ipairs(supported) do
         opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
-        -- TODO: Below has a warning, but it works. Why? No idea.
+	---@diagnostic disable-next-line: param-type-mismatch
         table.insert(opts.formatters_by_ft[ft], "prettier")
       end
     end,
