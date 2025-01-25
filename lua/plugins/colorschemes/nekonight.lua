d8cda56a606097454340f4432f84cf011350a486
Dap9 <tandonjayant@gmail.com>
2025-01-25 11:20:45 -0800

    Add nekonight, but disabled it because of issues

--- lua/plugins/colorschemes/nekonight.lua
+++ lua/plugins/colorschemes/nekonight.lua
@@ -1,17 +1,18 @@
--- This is fucked, not sure why. `opts.style` seems to be coming as a boolean for some reason.
 local name = "nekonight";
 
 local M = {
---   "neko-night/nvim",
---   name = name,
---   lazy = vim.g.colorscheme ~= name,
---   priority = 1000,
---   opts = {
---     style = "night",
---   },
---   config = function (_, opts)
---     require("utils").setup_colorscheme(name, opts);
---   end
+  "BrunoCiccarino/nekonight",
+  name = name,
+  -- This is fucked, not sure why. `opts.style` seems to be coming as a boolean for some reason.
+  enabled = false,
+  lazy = vim.g.colorscheme ~= name,
+  priority = 1000,
+  opts = {
+    style = "night",
+  },
+  config = function (_, opts)
+    require("utils").setup_colorscheme(name, opts);
+  end
 }
 
 return M
