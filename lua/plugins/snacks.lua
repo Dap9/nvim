ccc416ce9b6021e043a8c55172a424e6c47edbf6
Dap9 <tandonjayant@gmail.com>
2025-02-27 20:41:30 -0800

    Update snacks config to use `git-delta` and some keybinding changes.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -13,7 +13,22 @@ local M = {
       enabled = true,
       timeout = 3000,
     },
-    picker = { enabled = true },
+    picker = {
+      enabled = true,
+      ---@class snacks.picker.previewers.Config
+      previewers = {
+        diff = {
+          builtin = false,
+          cmd = { "delta" },
+        },
+      },
+      layout = {
+        cycle = true,
+        preset = function()
+          return vim.o.columns >= 120 and "bottom" or "vertical"
+        end,
+      },
+    },
     quickfile = { enabled = true },
     scope = { enabled = true },
     scroll = { enabled = false },
@@ -28,7 +43,7 @@ local M = {
   keys = {
     -- Top Pickers & Explorer
     {
-      "<leader><space>",
+      "<leader>sf",
       function()
         Snacks.picker.smart()
       end,
@@ -120,13 +135,13 @@ local M = {
       end,
       desc = "Git Branches",
     },
-    {
-      "<leader>gl",
-      function()
-        Snacks.picker.git_log()
-      end,
-      desc = "Git Log",
-    },
+    -- {
+    --   "<leader>gl",
+    --   function()
+    --     Snacks.picker.git_log()
+    --   end,
+    --   desc = "Git Log",
+    -- },
     {
       "<leader>gL",
       function()
@@ -134,34 +149,34 @@ local M = {
       end,
       desc = "Git Log Line",
     },
-    {
-      "<leader>gs",
-      function()
-        Snacks.picker.git_status()
-      end,
-      desc = "Git Status",
-    },
-    {
-      "<leader>gS",
-      function()
-        Snacks.picker.git_stash()
-      end,
-      desc = "Git Stash",
-    },
-    {
-      "<leader>gd",
-      function()
-        Snacks.picker.git_diff()
-      end,
-      desc = "Git Diff (Hunks)",
-    },
-    {
-      "<leader>gf",
-      function()
-        Snacks.picker.git_log_file()
-      end,
-      desc = "Git Log File",
-    },
+    -- {
+    --   "<leader>gs",
+    --   function()
+    --     Snacks.picker.git_status()
+    --   end,
+    --   desc = "Git Status",
+    -- },
+    -- {
+    --   "<leader>gS",
+    --   function()
+    --     Snacks.picker.git_stash()
+    --   end,
+    --   desc = "Git Stash",
+    -- },
+    -- {
+    --   "<leader>gd",
+    --   function()
+    --     Snacks.picker.git_diff()
+    --   end,
+    --   desc = "Git Diff (Hunks)",
+    -- },
+    -- {
+    --   "<leader>gf",
+    --   function()
+    --     Snacks.picker.git_log_file()
+    --   end,
+    --   desc = "Git Log File",
+    -- },
     -- Grep
     {
       "<leader>sb",
