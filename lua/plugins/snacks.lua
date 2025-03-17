de4a0bdff5eb4b545b97f2d966954fb7b90e2f11
Dap9 <tandonjayant@gmail.com>
2025-03-17 00:55:52 -0700

    Remove snacks explorer

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,7 +6,7 @@ local M = {
   opts = {
     bigfile = { enabled = true },
     dashboard = { enabled = true },
-    explorer = { enabled = true },
+    explorer = { enabled = false },
     indent = { enabled = true },
     input = { enabled = true },
     notifier = {
