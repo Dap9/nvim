b88cb2dea49c4bbfbfaf6f0a637dc001961988a6
Dap9 <tandonjayant@gmail.com>
2025-03-26 01:07:02 -0700

    Make smart-splits not be lazy loaded

--- lua/plugins/smart_splits.lua
+++ lua/plugins/smart_splits.lua
@@ -1,37 +1,41 @@
 local M = {
   "mrjones2014/smart-splits.nvim",
-  keys = {
-    {
-      [[<A-h>]],
-      function()
-        require("smart-splits").move_cursor_left()
-      end,
-    },
-    {
-      [[<A-j>]],
-      function()
-        require("smart-splits").move_cursor_down()
-      end,
-    },
-    {
-      [[<A-j>]],
-      function()
-        require("smart-splits").move_cursor_up()
-      end,
-    },
-    {
-      [[<A-l>]],
-      function()
-        require("smart-splits").move_cursor_right()
-      end,
-    },
-    {
-      [[<A-r>]],
-      function()
-        require("smart-splits").start_resize_mode()
-      end,
+  -- keys = {
+  --   -- {
+  --   --   "<A-
+  --   -- }
+  --   -- This fucks up mappings if nested
+  --   -- {
+  --   --   [[<A-r>",
+  --   --   function()
+  --   --     require("smart-splits").start_resize_mode()
+  --   --   end,
+  --   -- },
+  -- },
+
+  opts = {
+    resize_mode = {
+      silent = true,
+      hooks = {
+        on_enter = function()
+          vim.notify("Entering resize mode")
+        end,
+        on_leave = function()
+          vim.notify("Exiting resize mode, bye")
+        end,
+      },
     },
   },
+
+  config = function(_, opts)
+    local utils = require("utils")
+    utils.map({ "n" }, "<A-h>", require("smart-splits").move_cursor_left)
+    utils.map({ "n" }, "<A-j>", require("smart-splits").move_cursor_down)
+    utils.map({ "n" }, "<A-k>", require("smart-splits").move_cursor_up)
+    utils.map({ "n" }, "<A-l>", require("smart-splits").move_cursor_right)
+
+    require("smart-splits").setup(opts)
+  end,
 }
 
 return M
