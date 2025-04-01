0ec1308a0847d43dd1379b66c11419c9fa213fa8
Dap9 <tandonjayant@gmail.com>
2025-04-01 00:45:22 -0700

    Update versions + add blink mappings + add symbols.nvim for **potential** aerial replacement

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -19,7 +19,11 @@ local M = {
     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = { preset = "default" },
+    keymap = {
+      preset = "default",
+      ['<C-j>'] = { "select_next", "fallback" },
+      ['<C-k>'] = { "select_prev", "fallback" },
+    },
 
     completion = {
       accept = {},
