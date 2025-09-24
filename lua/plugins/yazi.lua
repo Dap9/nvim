0a26dbdd9198c8d5ab95a10dbeb9b0a7cb68cd58
Jayant Tandon <jayant@etched.com>
2025-09-23 18:46:03 -0700

    Update yazi keys

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -4,19 +4,13 @@ local M = {
   keys = {
     -- 👇 in this section, choose your own keymappings!
     {
-      "<leader>-",
+      "<leader>y",
       mode = { "n", "v" },
       "<cmd>Yazi<cr>",
       desc = "Open yazi at the current file",
     },
     {
-      -- Open in the current working directory
-      "<leader>cw",
-      "<cmd>Yazi cwd<cr>",
-      desc = "Open the file manager in nvim's working directory",
-    },
-    {
-      "<c-up>",
+      "<leader>yr",
       "<cmd>Yazi toggle<cr>",
       desc = "Resume the last yazi session",
     },
