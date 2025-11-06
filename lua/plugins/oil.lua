06163f47fad6e8d0b0e24258cb05dabf8a7c1cae
Dap9 <tandonjayant@gmail.com>
2025-11-06 02:38:47 -0800

    Remove oil.nvim, i barely use it now since i have yazi

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,6 +1,7 @@
 -- View & modify directories like a vim buffer
 local M = {
   "stevearc/oil.nvim",
+  enabled = false,
   -- https://github.com/stevearc/oil.nvim/issues/300
   -- Close enough
   cmd = "Oil",
